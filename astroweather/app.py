2304772e39ad3e4b4cfec0ee453cdb2b6999464d
martinw89 <403999+martinw89@users.noreply.github.com>
2025-01-09 22:12:14 -0600

    Isolate out xarray selecting part of getting a forecast

--- astroweather/app.py
+++ astroweather/app.py
@@ -12,9 +12,13 @@ async def index() -> str:
 
 @get("/api/forecast/")
 async def get_forecast(lat: float, lon: float) -> dict["str", list[int]]:
-    desired_x, desired_y = astro_data_reader.proj.transformer.transform(lon, lat)
-    nearest_point = ds.sel(x=desired_x, y=desired_y, method="nearest")
+    nearest_point = _select_forecast(lat, lon)
     return {"Seeing": [int(x) for x in nearest_point.Seeing.values], 
             "Transparency": [int(x) for x in nearest_point.Transparency.values]}
+
+def _select_forecast(lat: float, lon: float) -> xr.DataArray:
+    desired_x, desired_y = astro_data_reader.proj.transformer.transform(lon, lat)
+    nearest_point = ds.sel(x=desired_x, y=desired_y, method="nearest")
+    return nearest_point
 
 app = Litestar([index, get_forecast])
